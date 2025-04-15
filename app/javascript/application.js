ba656b8be87703984d9d0dae4b61c0d61c9fc55a
Sam Bodde <sambodde@Sams-MacBook-Air.local>
2025-04-15 15:11:29 -0400

    I have laid the foundation of our amazing tool, one small step for mankind

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/HelloWorld'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('react-root')
+  if (root) {
+    ReactDOM.render(<HelloWorld />, root)
+  }
+})
---
+++ app/javascript/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div className="hello-world">
+      <h1>Hello from React!</h1>
+      <p>This is a React component in a Rails application.</p>
+    </div>
+  )
+}
+
+export default HelloWorld 
